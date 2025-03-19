5afa570be765dc86f99b825fc73147ef1802fbd3
Adrian Vazquez <100007005.joan23@fje.edu>
2025-03-19 10:22:39 +0100

    Cambios pagina index y puntos admin

---
+++ Projecte_04/app/Http/Controllers/LugarController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Lugar;
+
+class LugarController extends Controller
+{
+    public function index()
+    {
+        $lugares = Lugar::with('etiquetas')->get();
+        return view('admin.puntos', compact('lugares'));
+    }
+}
